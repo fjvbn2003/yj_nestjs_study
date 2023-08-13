13db129a86637f30f152b3fbd359582918fd5c25
fjvbn2003 <fjvbn2003@gmail.com>
2023-08-13 22:58:32 +0900

    ADD routers

---
+++ miniproject1(passport,oauth)/src/routes/users.router.js
@@ -0,0 +1,49 @@
+const express = require("express");
+const passport = require("passport");
+const User = require("../models/users.model");
+const usersRouter = express.Router();
+
+usersRouter.post("/login", (req, res, next) => {
+  passport.authenticate("local", (err, user, info) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.json({ msg: info });
+    }
+    req.logIn(user, function (err) {
+      if (err) {
+        return next(err);
+      }
+      res.redirect("/");
+    });
+  })(req, res, next);
+});
+
+usersRouter.post("/logout", (req, res, next) => {
+  req.logOut(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/login");
+  });
+});
+usersRouter.post("/signup", async (req, res) => {
+  // user 객체를 생성
+  const user = new User(req.body);
+  try {
+    await user.save();
+    res.redirect("/login");
+  } catch (error) {
+    console.error(error);
+  }
+});
+usersRouter.get("/google", passport.authenticate("google"));
+usersRouter.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successReturnToOrRedirect: "/",
+    failureRedirect: "/login",
+  })
+);
+module.exports = usersRouter;
--- miniproject1(passport,oauth)/src/server.js
+++ miniproject1(passport,oauth)/src/server.js
@@ -1,15 +1,13 @@
 const express = require("express");
 const path = require("path");
 const { default: mongoose } = require("mongoose");
-const User = require("./models/users.model");
 const passport = require("passport");
-
-require("dotenv").config();
+const config = require("config");
+const serverConfig = config.get("server");
 const cookieSession = require("cookie-session");
-const {
-  checkAuthenticated,
-  checkNotAuthenticated,
-} = require("./middlewares/auth");
+const usersRouter = require("./routes/users.router");
+const mainRouter = require("./routes/main.router");
+require("dotenv").config();
 
 const app = express();
 const cookieEncryptionKey = process.env.COOKIE_ENCRYPTION_KEY;
@@ -38,7 +36,8 @@ require("./config/passport");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false })); // Html form의 데이터를 가져오기 위해서 필요.
-
+app.use("/", mainRouter);
+app.use("/auth", usersRouter);
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -53,65 +52,6 @@ mongoose
   });
 
 app.use("/static", express.static(path.join(__dirname, "public")));
-app.get("/", checkAuthenticated, (req, res) => {
-  res.render("index");
-});
-app.get("/login", checkNotAuthenticated, (req, res) => {
-  res.render("login");
-});
-app.post("/login", (req, res, next) => {
-  passport.authenticate("local", (err, user, info) => {
-    if (err) {
-      return next(err);
-    }
-    if (!user) {
-      return res.json({ msg: info });
-    }
-    req.logIn(user, function (err) {
-      if (err) {
-        return next(err);
-      }
-      res.redirect("/");
-    });
-  })(req, res, next);
-});
-
-app.get("/signup", checkNotAuthenticated, (req, res) => {
-  res.render("signup");
-});
-
-app.post("/logout", (req, res, next) => {
-  req.logOut(function (err) {
-    if (err) {
-      return next(err);
-    }
-    res.redirect("/login");
-  });
-});
-app.post("/signup", async (req, res) => {
-  // user 객체를 생성
-  const user = new User(req.body);
-  try {
-    await user.save();
-    return res.status(200).json({
-      success: true,
-    });
-  } catch (error) {
-    console.error(error);
-  }
-});
-
-app.get("/auth/google", passport.authenticate("google"));
-app.get(
-  "/auth/google/callback",
-  passport.authenticate("google", {
-    successReturnToOrRedirect: "/",
-    failureRedirect: "/login",
-  })
-);
-
-const config = require("config");
-const serverConfig = config.get("server");
 
 const port = serverConfig.port;
 app.listen(port, () => {
