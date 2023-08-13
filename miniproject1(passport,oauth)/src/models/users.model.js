3c15ffac26a5e16b69c2efa8119c959db41cc4be
fjvbn2003 <fjvbn2003@gmail.com>
2023-08-13 17:13:57 +0900

    ADD password encryption

--- miniproject1(passport,oauth)/src/models/users.model.js
+++ miniproject1(passport,oauth)/src/models/users.model.js
@@ -16,16 +16,30 @@ const userSchema = mongoose.Schema({
     sparse: true,
   },
 });
+const saltRounds = 10;
+userSchema.pre("save", function (next) {
+  let user = this;
+  // 비밀번호가 변경될때만
+  if (user.isModified("password")) {
+    // salt를 생성합니다.
+    bcrypt.genSalt(saltRounds, function (err, salt) {
+      if (err) return next(err);
+      bcrypt.hash(user.password, salt, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+      });
+    });
+  }
+});
 
 userSchema.methods.comparePassword = function (plainPassword, cb) {
   //bcrypt compare 비교
   // plain passord = > clioent, this.password => 데이터베이스에 있는 비밀번호
-  if (plainPassword === this.password) {
-    cb(null, true);
-  } else {
-    cb(null, false);
-  }
-  return cb({ error: "password error" });
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
 };
 
 const User = mongoose.model("User", userSchema);
