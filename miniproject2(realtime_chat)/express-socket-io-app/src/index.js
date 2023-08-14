28e17fbfbdbc6026f24563e587106ee0f3b99acc
fjvbn2003 <fjvbn2003@gmail.com>
2023-08-15 02:45:51 +0900

    ADD socket connect disconnect event logging

--- miniproject2(realtime_chat)/express-socket-io-app/src/index.js
+++ miniproject2(realtime_chat)/express-socket-io-app/src/index.js
@@ -1,9 +1,24 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
+
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("socket", socket.id);
+  socket.on("join", () => {});
+  socket.on("sendMessage", () => {});
+  socket.on("disconnect", () => {
+    console.log("socket disconnected", socket.id);
+  });
+});
+
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 const port = 4000;
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
