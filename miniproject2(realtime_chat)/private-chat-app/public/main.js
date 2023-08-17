df1826dfca29846802c9b5b52cedf2780ff56e83
fjvbn2003 <fjvbn2003@gmail.com>
2023-08-18 03:02:53 +0900

    ADD socket.io to express app

---
+++ miniproject2(realtime_chat)/private-chat-app/public/main.js
@@ -0,0 +1,17 @@
+const socket = io("http://localhost:4000", {
+  autoConnect: false,
+});
+
+socket.onAny((event, ...args) => {
+  console.log(event, ...args);
+});
+
+// 전역변수들
+const chatBody = document.querySelector(".chat-body");
+const userTitle = document.querySelector("user-title");
+const loginContainer = document.querySelector(".login-container");
+const userTalbe = document.querySelector(".users");
+const userTaglin = document.querySelector("#users-tagline");
+const tile = document.querySelector("#active-user");
+const messages = document.querySelector(".messages");
+const msgDiv = document.querySelector(".msg-form");
